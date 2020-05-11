3d1335932c62ac0a4ce4859a6d961dee7aaa419f
pozm <44528100+pozm@users.noreply.github.com>
2020-05-11 02:04:44 +0100

    Bot statuses

--- src/e/ready.ts
+++ src/e/ready.ts
@@ -1,7 +1,11 @@
 import { Client, Message } from "discord.js"
+import { status } from "../m/config";
+import { getrnd } from "../m/func";
 
 module.exports = function run(client :Client) : void {
 
     console.log('bot is now ready.')
+    
+    setInterval( ()  => {let data = status[getrnd(0,status.length-1)]; client.user!.setActivity( {name : data.title, type : data.type} ) }, 15e3)
 
 }
--- src/m/config.ts
+++ src/m/config.ts
@@ -1,2 +1,5 @@
+import { ActivityType } from "discord.js";
+
 export const Prefix = '.';
 export const Owner = ["288062966803333120","518763902570594314"]
+export const status = [{title : 'test', type : 'WATCHING' as ActivityType}]
