d6e218657500ce3e16b5a30e25c282d1a0ff5e44
pozm <44528100+pozm@users.noreply.github.com>
2020-05-15 02:40:06 +0100

    + NSFW commands - hentai ( ͡° ͜ʖ ͡°)

---
+++ src/c/nsfw/hentai.ts
@@ -0,0 +1,72 @@
+import { Command, CommandArgument, CommandArgTypes} from "../../m/class";
+import { Client, Message, GuildMember, MessageEmbed, MessageAttachment } from "discord.js";
+import got from "got";
+import {getrnd} from "../../m/func";
+
+let stored : any
+let cannew = true
+
+function getFile(jsn : any) : any {
+	let dat = jsn.data
+	let meta = jsn.meta
+	let cat =  Math.floor( Math.random() * (dat.length-1) )
+	//console.log(cat,dat[cat])
+	let ind = dat[cat][Math.floor( Math.random() * (dat[cat].length-1) )]
+	if(!ind) return getFile(jsn)
+	return ind
+
+}
+
+
+module.exports = class test extends Command
+{
+
+	constructor(  )
+	{
+
+		super
+		(
+			{
+				Name  : 'hentai',
+				Desc  : 'Gets a random hentai image.',
+				Guild : false,
+				Owner : false,
+				Hidden: true,
+				nsfw  : true,
+			}
+		)
+
+	}
+
+	public run = async (message : Message, client : Client, args?: {name : string, value : CommandArgTypes}[] ) => {
+		console.log(!cannew? 'Data is stored in cache so it\'s being used' : 'Data isn\'t stored in cache so going to request and cache.')
+		if (cannew) {
+			cannew = false
+			let index = getrnd(0, 2000)
+			console.log('sending request', index)
+			let res = (await got(`https://hr.hanime.tv/api/v8/community_uploads?channel_name__in[]=nsfw-general&__offset=${index}&__order=created_at,DESC&loc=https://hanime.tv`)).body
+			if (!res) return {
+				Worked: false,
+				Error: new Error('There was an issue contacting the api, try again later?')
+			}
+			let jsn = JSON.parse(res)
+			if (!jsn) return {
+				Worked: false,
+				Error: new Error('There was an issue contacting the api, try again later?')
+			}
+			if (!stored) {stored = jsn;stored.data = [stored.data]} else { if (stored.data.indexOf([jsn.data]) < 0) stored.data = [...stored.data, [...jsn.data]]; else console.log('Already indexed..')}
+			stored.meta = jsn.meta
+			setTimeout( () => {cannew = true},5e3 )
+			setTimeout( () => {stored.data.splice(0,1)},15e3 )
+		}
+		let jsn = stored
+		console.log(jsn.data.map((v:any)=>v.map((v2:any) => v2.id ) ) )
+		let meta = jsn.meta
+		if(meta.error) {console.error(meta.error); return {Worked : false,Error:new Error('There was an issue contacting the api, try again later?')}}
+		let dat = getFile(jsn)
+
+		await message.channel.send( new MessageEmbed().setImage(dat.url) )
+		return {Worked : true}
+	}
+
+}
