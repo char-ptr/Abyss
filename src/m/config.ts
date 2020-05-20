8a82f1636aba1263aa02ac58cb9503b28d2c55bc
pozm <44528100+pozm@users.noreply.github.com>
2020-05-20 17:27:05 +0100

    Change prefix if testing

--- src/m/config.ts
+++ src/m/config.ts
@@ -1,5 +1,5 @@
 import { ActivityType } from "discord.js";
 
-export const Prefix = '.';
+export const Prefix = !process.env.test ? '.' : ';';
 export const Owner = ["288062966803333120","518763902570594314"]
 export const status = [{title : 'test', type : 'WATCHING' as ActivityType}]
