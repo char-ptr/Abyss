f6c1afe0e67fbf80c0fd2e1aeb4619420ee5891b
pozm <44528100+pozm@users.noreply.github.com>
2020-07-21 03:13:35 +0100

    new status.

--- src/m/config.ts
+++ src/m/config.ts
@@ -1,5 +1,5 @@
-import { ActivityType } from "discord.js";
+import {ActivityType} from "discord.js";
 
 export const Prefix = !process.env.test ? '.' : ';';
 export const Owner = ["288062966803333120","518763902570594314"]
-export const status = [{title : 'test', type : 'WATCHING' as ActivityType}]
+export const status = [{title : '.help', type: 'WATCHING' as ActivityType}]
