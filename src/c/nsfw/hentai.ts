264f14249619b5b66ca77a1e4ae025ef2eea964b
pozm <44528100+pozm@users.noreply.github.com>
2020-08-31 01:19:48 +0100

    read from file

--- src/c/nsfw/hentai.ts
+++ src/c/nsfw/hentai.ts
@@ -1,11 +1,17 @@
-import {Command, CommandArgTypes} from "../../m/class";
+import {Command, CommandArgTypes, CommandArgument} from "../../m/class";
 import {Client, Message, MessageEmbed} from "discord.js";
-import { readdirSync } from "fs";
+import { existsSync, readdirSync, readFileSync } from "fs";
 import { join } from "path";
+let txt : Buffer
+if (existsSync(join(__dirname,'../../../Util/hData.json'))) {
 
-const dir = 'D:\\Media\\Pictures\\Web documentation\\Hentai\\Normal'
+	txt = readFileSync(join(__dirname,'../../../Util/hData.json'))
+}else{
+	txt = Buffer.from(JSON.stringify({'Normal':[],'Yuri':[]}))
+}
+let files : {[ley:string] : string[], Normal:string[],Yuri:string[]} = JSON.parse(txt.toString('utf8'))
 
-let files = readdirSync(dir)
+
 
 module.exports = class test extends Command
 {
@@ -22,7 +28,33 @@ module.exports = class test extends Command
 				Owner : false,
 				Hidden: true,
 				Nsfw  : true,
-				Alias : ['hen']
+				Alias : ['hen'],
+				Args : [
+					new CommandArgument({
+						Name:'amount',
+						Needed:false,
+						Type:'num',
+						AltNames:['a']
+					}),
+					new CommandArgument({
+						Name:'sort',
+						Needed:false,
+						Type:'str',
+						AltNames:['s']
+					}),
+					new CommandArgument({
+						Name:'type',
+						Needed:false,
+						Type:'str',
+						AltNames:['t']
+					}),
+					new CommandArgument({
+						Name:'many',
+						Needed:false,
+						Type:'bool',
+						AltNames:['m']
+					}),
+				]
 			}
 		)
 
@@ -30,9 +62,65 @@ module.exports = class test extends Command
 
 	public run = async (message : Message, client : Client, args?: {name : string, value : CommandArgTypes}[] ) => {
 
-		message.channel.send({files:[
-			join(dir, '/'+files[ Math.floor(Math.random() * files.length)])
-		]}).catch(r=>{message.channel.send('Unfortunately i was unable to send, this is likely due to the file being too large.');console.log(r)})
+		if (this.GetArg('many',args!)) {
+			message.channel.send(`There is currently ${files['Normal'].length + files['Yuri'].length} files in DB`)
+			return {Worked:true}
+		}
+
+		const generateBySort = (sort:string,file:string[],index?:number) : string =>{
+			switch (sort) {
+				case ('first'):
+					return file[index??0]
+				break;
+				case ('last'):
+					return file[file.length-1 - (index ??0) ]
+				break;
+				case('random'):
+					return file[ Math.floor(Math.random() * file.length)]
+				break;
+				default : return ''; break;
+			}
+		} 
+		
+		const generateByType = (type:string) : string =>{
+			switch (type) {
+				case ('normal'):
+					return 'Normal'
+				break;
+				case ('yuri'):
+					return 'Normal'
+				break;
+				default : return 'Normal'; break;
+			}
+		} 
+
+		let filesu : string[] = [];
+
+		let convsdat = new Map([['r','random'],['l','last'],['f','first']])
+		let convsort = (t:string)=> convsdat.get(t) ?? t
+		
+		let convtdat = new Map([['n','normal'],['y','yuri']])
+		let convType = (t:string)=> convtdat.get(t) ?? t
+
+		let amount = this.GetArg('amount',args!)
+		let sort = convsort (this.GetArg('sort',args!))
+		let type = generateByType (convType (this.GetArg('type',args!)))
+		console.log(sort)
+
+
+
+		if (! sort){
+			console.log('Automatically converting to random');
+			sort = 'random'
+		}
+		for (let i =0;i<(amount ?? 1);i++){
+			filesu = [
+				...filesu,
+				generateBySort(sort,files[type],i)
+			]
+		}
+		console.log(filesu,amount)
+		message.channel.send({files:filesu}).catch(r=>{message.channel.send('Unfortunately i was unable to send, this is likely due to the file being too large.');console.log(r)})
 
 		return {Worked:true}
 	}
